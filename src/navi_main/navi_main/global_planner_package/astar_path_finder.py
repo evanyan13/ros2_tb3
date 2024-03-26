306e0ea20f5fc76ffd041b1375a0b493b0b01697
evanyan13 <weidongy201@gmail.com>
2024-03-26 12:52:54 +0000

    Edit astar implmentation:

--- src/navi_main/navi_main/global_planner_package/astar_path_finder.py
+++ src/navi_main/navi_main/global_planner_package/astar_path_finder.py
@@ -22,57 +22,57 @@ def find_astar_path(map: GlobalMap, start: GlobalPlannerNode, goal: GlobalPlanne
         print("We are already at the destination")
         return []
 
-    no_path = True
     open_nodes = []
-    found_path = []
-    visited_nodes = set()
+    open_set = set()
+    visited_set = set()
+
     start.g = 0
     start.h = start.calculate_distance(goal)
     start.f = start.g + start.h
     heapq.heappush(open_nodes, (start.f, start))
+    open_set.add((start.x, start.y))
  
     while open_nodes:
-        current_node = heapq.heappop(open_nodes)[1]
+        _, current_node = heapq.heappop(open_nodes)
+        open_set.remove((current_node.x, current_node.y))
         print(f"\nProcessing Node: ({current_node.x}, {current_node.y}), f: {current_node.f}")
 
-        # Check if current node has been visited before
-        if (current_node.x, current_node.y) in visited_nodes:
+        # Skip if current node has been visited before
+        if (current_node.x, current_node.y) in visited_set:
             continue
 
+        # Return when current node is the goal
         if current_node.equals(goal):
-            no_path = False
             print(f"Goal reached: ({current_node.x}, {current_node.y})")
-            found_path = current_node.backtrack_path()
-            break
+            return current_node.backtrack_path()
 
-        visited_nodes.add((current_node.x, current_node.y))
+        visited_set.add((current_node.x, current_node.y))
 
         for neighbour in current_node.generate_neighbours(map.resolution):
-            print(f"Checking neighbour: ({neighbour.x}, {neighbour.y})")
+            n_index = neighbour.x, neighbour.y
+            print(f"Checking neighbour: {n_index}")
+            if n_index in visited_set:
+                continue
 
             if map.is_node_valid(neighbour) and map.is_node_avail(neighbour):
                 g_new = current_node.g + current_node.calculate_distance(neighbour)
                 h_new = neighbour.calculate_distance(goal)
                 f_new = g_new + h_new
 
-                in_open_nodes = any(n.x == neighbour.x and n.y == neighbour.y for _, n in open_nodes)
-
-                if (f_new < neighbour.f) or not in_open_nodes:
+                if (f_new < neighbour.f) or n_index not in open_set:
                     neighbour.g = g_new
                     neighbour.h = h_new
                     neighbour.f = f_new
                     neighbour.parent = current_node
 
-                    if not in_open_nodes:
+                    if n_index not in open_set:
                         heapq.heappush(open_nodes, (neighbour.f, neighbour))
+                        open_set.add(n_index)
                     else:
-                        visited_nodes.add((neighbour.x, neighbour.y))
+                        visited_set.add(n_index)
 
-    # Else returns empty list
-    if no_path:
-        print("Path not found")
-        return []
-    return found_path
+    print("Path not found")
+    return []
 
 def print_path(path: list):
     print([(n.x, n.y) for n in path])
