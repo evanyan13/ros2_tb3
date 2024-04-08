800ea4b198c468e56ebc8d32052f6a5ab585c480
LeeChuan2004 <e1129853@nus.edu.sg>
2024-04-08 22:40:43 +0800

    Edited Line follower code and colour sensor code

--- src/line_follower/line_follower/__init__.py
+++ src/line_follower/line_follower/__init__.py
@@ -5,63 +5,78 @@ from std_msgs.msg import String
 import RPi.GPIO as GPIO
 import time
 
+
 class LineFollower(Node):
     def __init__(self):
         super().__init__('line_follower')
+        self.setup()
         self.publisher_cmd_vel = self.create_publisher(Twist,'cmd_vel',10)
         self.subscriber_color = self.create_subscription(String, 'color_info', self.color_callback, 10)
-        self.setup()
         self.line_following = False
         self.sensor_left = 0
         self.sensor_right = 0
-        
+
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         self.IR_left = 3 #Rpi pin 5
         self.IR_right = 27 #Rpi pin 13
         GPIO.setup(self.IR_left,GPIO.IN)
         GPIO.setup(self.IR_right,GPIO.IN)
-    
+
     def color_callback(self,msg):
         if msg.data == 'RED' and not self.line_following:
             self.start_line_following()
-            
+
     def start_line_following(self):
+        print("start_line_following called")
         self.line_following = True
         twist = Twist()
-        twist.linear.x = 0.5
-        twist.angular.z = 0.3
-        time.sleep(0.5)
-        twist.angular.z = -0.6
-        time.sleep(0.5) 
+        while True:
+            self.sensor_left = GPIO.input(self.IR_left)
+            self.sensor_right = GPIO.input(self.IR_right)
+            if(self.sensor_left or self.sensor_right):
+                break
+            else: 
+                #twist.linear.x = 0.05
+                #twist.angular.z = 0.1
+                #time.sleep(0.5)
+                #twist.angular.z = -0.1
+                #time.sleep(0.5)
+                #print("Wiggle")
         while self.line_following:
             self.sensor_left = GPIO.input(self.IR_left)
             self.sensor_right = GPIO.input(self.IR_right)
-            if self.sensor_left and self.sensor_right:
-                twist.linear.x = 0.5
-                twist.angular.z = 0
+            if  self.sensor_left and self.sensor_right:
+                twist.linear.x = 0.11
+                twist.angular.z = 0.0
+                print("both in black")
             elif self.sensor_left and not self.sensor_right:
                 twist.angular.z = 0.2
-                twist.linear.x = 0
+                twist.linear.x = 0.
+                print("Right in black")
             elif self.sensor_right and not self.sensor_left:
                 twist.angular.z = -0.2
-                twist.linear.x = 0
+                twist.linear.x = 0.0
+                print("Left in black")
             else:
+                print("both not in black")
                 self.check_end_line()
             self.publisher_cmd_vel.publish(twist)
-    
+
     def check_end_line(self):
+        print("check end line started")
         if not self.sensor_left and not self.sensor_right:
             self.stop_line_follower()
-    
+
     def stop_line_follower(self):
         self.line_following = False
-        twist_stop = Twist()
-        twist_stop.linear.x = 0
-        twist_stop.angular.z = 0
-        self.publisher_cmd_vel.publish(twist_stop)
-        GPIO.cleanup()
-        
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = 0.0
+        self.publisher_cmd_vel.publish(twist)
+        self.get_logger().info(f"Twist: {twist.linear.x} & {twist.angular.z}")
+        print("stopped")
+
 def main(args=None):
     rclpy.init(args=args)
     line_follower = LineFollower()
@@ -69,11 +84,9 @@ def main(args=None):
         rclpy.spin(line_follower)
     except KeyboardInterrupt:
         line_follower.stop_line_follower()
-    
-    line_follower.destroy_node()
-    rclpy.shutdown
+        time.sleep(1)
+        line_follower.destroy_node()
+        rclpy.shutdown
 
 if __name__ == '__main__':
-    main()    
-                
-     
+    main()  
--- src/marker/marker/__init__.py
+++ src/marker/marker/__init__.py
@@ -7,43 +7,48 @@ import time
 class ColorSensor(Node):
     def __init__(self):
         super().__init__('color_sensor')
+        self.setup()
         self.publisher_color = self.create_publisher(String, 'color_info', 10)
-        self.timer = self.create_timer(3,self.color_detect) #publishes every 3 sec
-        
+        self.create_timer(0.5, self.color_detect)
+
     def setup(self):
         GPIO.setmode(GPIO.BCM)
-        self.S2 = 22
-        self.S3 = 23
-        self.OUT = 24
-        GPIO.setup(self.S2,GPIO.OUT)
-        GPIO.setup(self.S3,GPIO.OUT)
-        GPIO.setup(self.OUT,GPIO.IN)
-        
-    def color_read_value(self,a0,a1):
+        self.s2 = 22
+        self.s3 = 23
+        self.out = 24
+        GPIO.setup(self.s2,GPIO.OUT)
+        GPIO.setup(self.s3,GPIO.OUT)
+        GPIO.setup(self.out,GPIO.IN)
+
+    def color_read_value(self, a0, a1):
         GPIO.output(self.s2, a0) 
         GPIO.output(self.s3, a1) 
-        # Give the sensor some time to adjust 
+        # Give the sensor some time to adjust
         time.sleep(0.1) 
-        # Wait for a full cycle (this will make sure we only count full cycles) 
-        GPIO.wait_for_edge(self.out, GPIO.FALLING) 
-        GPIO.wait_for_edge(self.out, GPIO.RISING) 
-        # print("waiting")
+        # Wait for a full cycle (this will make sure we only count full cycles)
+        GPIO.wait_for_edge(self.out, GPIO.FALLING)
+        # Measuring the time it takes for the output square wave to change from max value to min value
+        # Basically measuring how long one half of the period of the square wave is 
+        # because according to the spec, the output square wave is of 50% duty cycle with frequency directly proportional to light intensity
+        GPIO.wait_for_edge(self.out, GPIO.RISING)
         start = time.time() 
         GPIO.wait_for_edge(self.out, GPIO.FALLING) 
-        # The time that passed while we were waiting for the out to change
-        # print("returning successfully")
-        return (time.time() - start) * 1000000 
-        
-    
+        half_period = time.time() - start
+        return half_period * 1000000 
+
+
     def color_detect(self):
         color_val = String()
         while True:
-            # print("in loop")
-            r = self.color_read_value(GPIO.LOW, GPIO.LOW) 
+            r = self.color_read_value(GPIO.LOW, GPIO.LOW)
             time.sleep(0.1) 
             g = self.color_read_value(GPIO.HIGH, GPIO.HIGH) 
             time.sleep(0.1) 
-            b = self.color_read_value(GPIO.LOW, GPIO.HIGH) 
+            b = self.color_read_value(GPIO.LOW, GPIO.HIGH)
+            time.sleep(0.1) 
+            print(f"Red:{r}, Green:{g}, Blue:{b}")
+            # Larger the half period, larger the period, lower the frequency, lower the intensity for the particular colour.
+            # Smaller the half period, smaller the period, higher the frequency, higher the intensity for the particular colour.
             if (b < g) and (b < r): 
                 color_val.data = "BLUE"
             elif (g < b) and (g < r): 
@@ -52,21 +57,17 @@ class ColorSensor(Node):
                 color_val.data = "RED"
             time.sleep(1)
             self.publisher_color.publish(color_val)
+            self.get_logger().info(f"Published colour: {color_val.data}")
 
 def main(args=None):
     rclpy.init(args=args)
     color_sensor = ColorSensor()
-    rclpy.spin(color_sensor)
-    color_sensor.destroyNode()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(color_sensor)
+    except KeyboardInterrupt:
+        color_sensor.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-    
-       
 
-    
-    
-          
-          
-        
