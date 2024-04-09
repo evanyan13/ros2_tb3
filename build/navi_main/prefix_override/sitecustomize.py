da566d674984f28f4e19cdfe3dae9645850cf9f1
evanyan13 <weidongy201@gmail.com>
2024-04-09 18:24:27 +0000

    Remove build/log files

--- build/navi_main/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/evanyan13/colcon_ws/install/navi_main'
--- build/turtlebot3_gazebo/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/evanyan13/colcon_ws/build/turtlebot3_gazebo/turtlebot3_drive" "TARGETS" "turtlebot3_drive" "DESTINATION" "lib/turtlebot3_gazebo")
